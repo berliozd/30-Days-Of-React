f0ee73312ada200cef3498e1c9925922f31cd0fa
Didier Berlioz <berliozd@gmail.com>
2020-10-05 23:33:54 +0200

    day 2

---
+++ 01_Day_JavaScript_Refresher/introduction.js
@@ -0,0 +1,10 @@
+class Person {
+  constructor(firstName, lastName) {
+    this.firstName = firstName
+    this.lastName = lastName
+  }
+}
+
+const person1 = new Person('Asabeneh', 'Yetayeh')
+
+console.log(person1)
